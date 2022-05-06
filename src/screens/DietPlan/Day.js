3b135a5d3df190804b1cd56f759f5da406a4ba3f
InzamamAbbasii <abbasiinzamam2@gmail.com>
2022-05-07 04:14:09 +0500

    remove extra code

--- src/screens/DietPlan/Day.js
+++ src/screens/DietPlan/Day.js
@@ -4,9 +4,7 @@ import CommonStyle from '../../styles/CommonStyle';
 
 const Day = ({navigation, route}) => {
   const [daysList, setDaysList] = useState([]);
-  const helloWorld = () => {
-    alert('Hello World');
-  };
+
   useEffect(() => {
     setDaysList([]);
     if (route.params.duration === '1') {
